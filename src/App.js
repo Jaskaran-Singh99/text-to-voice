f42d11ff644a43f053dc9bc6bf6065d8d2de66eb
Jaskaran Singh <jaskaran7087065314@gmail.com>
2023-10-11 18:22:38 -0400

    final commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import { useState } from "react";
-import { useAsyncError } from "react-router-dom";
+
 function App() {
-  const [listen, setListen] = useState('')
+  
   const [textarea, setTextarea] = useState('')
   const textToSpeech = ()=>{
     let speech = new SpeechSynthesisUtterance()
     speech.text = textarea
     window.speechSynthesis.speak(speech)
+    console.log(speechSynthesis)
+  
+
   }
   return (
     <div className="hero">
       <h1>Text to speech <span className="span">converter</span></h1>
       <textarea placeholder="Enter the text" onChange={(e)=>setTextarea(e.target.value)} ></textarea>
       <div className="row">
-        <select></select>
+  
         <button onClick={()=>textToSpeech()}>Listen</button>
       </div>
-      {console.log(listen)}
+  
     </div>
   )
 }
